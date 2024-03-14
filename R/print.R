cfb05d90d734cd9cd627f9b8f4bd818ea3990b0b
seokhoonj <seokhoonj@gmail.com>
2024-03-14 14:44:29 +0900

    `draw_line()` function is added

---
+++ R/print.R
@@ -0,0 +1,19 @@
+#' Draw a line
+#'
+#' Draw a line in console.
+#'
+#' @param width a numeric vector specifying width (default: `options()$width`)
+#' @param mark a string specifying mark (default: "=")
+#' @return a string vector with repeated mark
+#'
+#' @examples
+#' \donttest{cat(draw_line())}
+#'
+#' @export
+draw_line <- function(width, mark = "=") {
+  if (missing(width))
+    width <- options()$width
+  sapply(width, function(x)
+    paste(rep(mark, times = ifelse(!is.na(x), min(x, options()$width), 0)),
+          collapse = ""))
+}
