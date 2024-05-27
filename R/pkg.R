1c0dfde3e379085c42b6e9a61fef9b0231bb1102
seokhoonj <seokhoonj@gmail.com>
2024-05-27 15:33:36 +0900

    `print_pkg_recipe()` function is added

---
+++ R/pkg.R
@@ -0,0 +1,24 @@
+print_pkg_recipe <- function() {
+  lines <- c(
+    "usethis::create_package(path)",
+    "usethis::use_description()",
+    sprintf("Title: %s", chatgtp::ask_chatgpt()),
+    "Description: This package is made by ",
+    "person(given = \"Seokhoon\", family = \"Joo\", email = \"seokhoonj@gmail.com\",\n       role = c(\"aut\", \"cre\"))",
+    "Depends: R (>= 3.5.0)",
+    "Remotes:\n  github::seokhoonj/ggshort\n  github::seokhoonj/jaid",
+    "Imports:\n  data.table (>= 1.14.2),\n  ggplot2 (>= 3.3.3),\n  ggshort,\n  jaid",
+    "usethis::use_namespace()",
+    "usethis::use_mit_license() # usethis::use_gpl3_license()",
+    "usethis::use_package_doc()",
+    "#' @description\n#' blah blah blah\n#' @keywords internal,\n#' @useDynLib jaid, .registration = TRUE\n#' @import ggplot2\n#' @importFrom Rcpp sourceCpp",
+    "usethis::use_readme_md()",
+    "usethis::use_cran_badge()",
+    "usethis::use_cran_comments()",
+    "# Change the environment to a terminal",
+    "# git add .",
+    "# git commit -m ",
+    "usethis::use_github_action_check_release()"
+  )
+  cat("path = ''", paste0("\n", lines))
+}
