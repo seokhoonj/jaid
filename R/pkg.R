3b88a102777874db2f8fe8834d3924084ff69ef6
seokhoonj <seokhoonj@gmail.com>
2025-09-12 21:23:13 +0900

    Add `.scaffold_deprecated()`

--- R/pkg.R
+++ R/pkg.R
@@ -1,20 +1,31 @@
 
 # Scaffold ----------------------------------------------------------------
 
-.scaffold_package_recipe <- function() {
+.scaffold_package_recipe <- function(path = "~/Dropbox/temp_package") {
   lines <- c(
-    "usethis::create_package(path)",
+    sprintf("usethis::create_package(%s)", path),
     "usethis::use_description()",
     sprintf("Title: %s", 'chatgtp::ask_chatgpt()'),
     sprintf("Description: %s", 'chatgtp::ask_chatgpt()'),
     "person(given = \"Seokhoon\", family = \"Joo\", email = \"seokhoonj@gmail.com\",\n       role = c(\"aut\", \"cre\"))",
     sprintf("Depends: R (>= %s)", getRversion()),
-    "Remotes:\n  github::seokhoonj/ggshort\n  github::seokhoonj/jaid",
-    "Imports:\n  data.table (>= 1.14.2),\n  ggplot2 (>= 3.3.3),\n  ggshort,\n  jaid",
+    "Remotes:",
+    "  github::seokhoonj/ggshort",
+    "  github::seokhoonj/jaid",
+    "Imports:",
+    "  data.table (>= 1.14.2)",
+    "  ggplot2 (>= 3.3.3),",
+    "  ggshort,",
+    "  jaid",
     "usethis::use_namespace()",
-    "usethis::use_mit_license() # usethis::use_gpl3_license()",
+    "usethis::use_mit_license() # usethis::use_apache_license(), usethis::use_gpl3_license()",
     "usethis::use_package_doc()",
-    "#' @description\n#' blah blah blah\n#' @keywords internal,\n#' @useDynLib jaid, .registration = TRUE\n#' @import ggplot2\n#' @importFrom Rcpp sourceCpp",
+    "#' @description",
+    "#' chatgpt::ask_chatgpt()",
+    "#' @keywords internal",
+    "#' @useDynLib jaid, .registration = TRUE",
+    "#' @import ggplot2",
+    "#' @importFrom Rcpp sourceCpp",
     "usethis::use_readme_md()",
     "usethis::use_cran_badge()",
     "usethis::use_cran_comments()",
@@ -53,26 +64,25 @@
 
 # Internal helper functions -----------------------------------------------
 
-.package_license <- function(packages) {
-  license <- sapply(packages, function(x) utils::packageDescription(x)["License"])
-  names(license) <- packages
-  license
-}
-
-.print_description_license <- function(packages = c("data.table", "ggplot2")) {
-  lines <- sort(unique(sapply(packages, function(x)
-    sprintf("%s (%s)", x, package_license(x)))))
-  cat("X-PackageLicense:", paste0("\n  ", lines))
-
-}
-
 .print_description_depends <- function() {
   cat(sprintf("Depends: R (>= %s)\n", getRversion()))
 }
 
 .print_description_imports <- function(packages = c("data.table", "ggplot2")) {
   lines <- sort(unique(sapply(packages, function(x)
-    sprintf("%s (>= %s)", x,utils::packageVersion(x)))))
+    sprintf("%s (>= %s)", x, utils::packageVersion(x)))))
   cat("Imports:", paste0("\n  ", lines))
+}
+
+.print_description_license <- function(packages = c("data.table", "ggplot2")) {
+  lines <- sort(unique(sapply(packages, function(x)
+    sprintf("%s (%s)", x, .package_license(x)))))
+  cat("X-PackageLicense:", paste0("\n  ", lines))
+}
+
+.package_license <- function(packages = c("data.table", "ggplot2")) {
+  license <- sapply(packages, function(x) utils::packageDescription(x)["License"])
+  names(license) <- packages
+  license
 }
 
